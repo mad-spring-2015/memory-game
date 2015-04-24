49799a9e61c2fc7e15978cc93dce319ffa3b241d
ayang10 <ayang014@gmail.com>
2015-04-24 19:58:45 -0400

    Added SplashFragment

---
+++ src/edu/uncc/mad/memorygame/SplashFragment.java
@@ -0,0 +1,52 @@
+package edu.uncc.mad.memorygame;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SplashFragment extends Fragment {
+
+	public SplashFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_splash, container, false);
+	}
+
+	// TODO: Rename method, update argument and hook method into UI event
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+	}
+
+	/**
+	 * This interface must be implemented by activities that contain this
+	 * fragment to allow an interaction in this fragment to be communicated to
+	 * the activity and potentially other fragments contained in that activity.
+	 * <p>
+	 * See the Android Training lesson <a href=
+	 * "http://developer.android.com/training/basics/fragments/communicating.html"
+	 * >Communicating with Other Fragments</a> for more information.
+	 */
+	public interface OnFragmentInteractionListener {
+		// TODO: Update argument type and name
+		public void onFragmentInteraction(Uri uri);
+	}
+
+}
