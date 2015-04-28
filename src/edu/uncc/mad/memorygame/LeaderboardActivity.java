aa21a6339ea34fdf4e2f9596dbd0b94dfa576b8e
ajayveee <ajayveee@gmail.com>
2015-04-28 14:18:30 -0400

    Added leaderboard functionality

--- src/edu/uncc/mad/memorygame/LeaderboardActivity.java
+++ src/edu/uncc/mad/memorygame/LeaderboardActivity.java
@@ -1,5 +1,6 @@
 package edu.uncc.mad.memorygame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.parse.FindCallback;
@@ -10,14 +11,19 @@ import com.parse.ParseUser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 public class LeaderboardActivity extends Activity {
 
+	private List<ParseUser> users = new ArrayList<ParseUser>();
+	private LeaderboardListAdapter adapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_leaderboard);
-		
+		ListView lv = (ListView) findViewById(R.id.listViewLb);
+		adapter = new LeaderboardListAdapter(this, R.layout.leaderboard_list_item, users);
+		lv.setAdapter(adapter);
 		ParseQuery<ParseUser> topUsers = ParseUser.getQuery();
 		topUsers.orderByDescending("score");
 		topUsers.findInBackground(new FindCallback<ParseUser>() {
@@ -28,7 +34,8 @@ public class LeaderboardActivity extends Activity {
 					Log.e(MemoryGame.LOGGING_KEY, "Error fetching users", e);
 					return;
 				}
-				
+				LeaderboardActivity.this.users.addAll(users);
+				adapter.notifyDataSetChanged();
 			}
 		});
 	}
--- src/edu/uncc/mad/memorygame/LeaderboardListAdapter.java
+++ src/edu/uncc/mad/memorygame/LeaderboardListAdapter.java
@@ -3,16 +3,18 @@ package edu.uncc.mad.memorygame;
 import java.util.List;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.parse.ParseUser;
 
 public class LeaderboardListAdapter extends ArrayAdapter<ParseUser> {
 
-	
 	private Context context;
 	private int resource;
-	private int textViewResourceId;
 	private List<ParseUser> users;
 	
 	public LeaderboardListAdapter(Context context, int resource, List<ParseUser> objects) {
@@ -21,5 +23,20 @@ public class LeaderboardListAdapter extends ArrayAdapter<ParseUser> {
 		this.resource= resource;
 		this.users = objects;
 	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView == null ){
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(resource, parent, false);
+		}
+		ParseUser user = users.get(position);
+		((TextView)convertView.findViewById(R.id.textViewLbUserFnameLname)).setText(user.getString("firstName") + " " + user.getString("lastName"));
+		((TextView)convertView.findViewById(R.id.textViewLbUsername)).setText(user.getUsername());
+		((TextView)convertView.findViewById(R.id.textViewLbScore)).setText(user.getInt("score") + "");
+		((TextView)convertView.findViewById(R.id.textViewLbLevel)).setText("Level : " + user.getInt("level"));
+		return convertView;
+	}
+	
 
 }
